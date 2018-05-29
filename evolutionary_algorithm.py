ade3707575d75a2dc79126d21abcb71caa0cc5b2
jan <jan-lucas@hetnet.nl>
2018-05-29 17:06:06 +0200

    parallelizes the evolutionary algorithm Adds approach with differing cost functions

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -1,5 +1,18 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from tkinter import TclError
+from multiprocessing import Pool
+
+p = Pool(4)
+
+
+def _async_evaluate(population, fit_func):
+    # Evaluate individual i
+    # print(("Evaluating individual {}: {}".format(i, population[i])))
+    x = p.map(fit_func, population)
+    # fitness = fit_func(population[i])
+    print("Fitness:{}".format(x))
+    return x
 
 
 class EvolutionaryAlgorithm:
@@ -16,64 +29,105 @@ class EvolutionaryAlgorithm:
         #                     0.005,   0.05,   0.02,   0.02,   0.02,   0.1])
         self.scale = init*0.5
 
-    
-        self.population = (np.random.random((pop_size, 10)) - 0.5) * self.scale + init
+        # scale = np.array([1000,     10000,      1000000,    0.05,
+        #                     0.05,   0.05,   0.02,   0.02,   0.02,   0.1])
+        scale = init * .05
+
+        # Init and scale are used to make the initial population.
+        # init is used as the location for the normal distribution
+        # and scale determines the width of the gaussian to sample
+
+        # init = np.random.normal(scale=10, size=pop_size)
+        # scale = np.abs(init * 0.2)
+
+        # self.population = (np.random.random((pop_size, 10)) - 0.5) * scale + init # Uniformly distributed init
+        self.population = np.random.normal(loc=init, scale=scale, size=(10, 10))  # normally distributed init
+
+        # Member definitions
         self.fit_func = fitness_function
         self.pop_size = pop_size
-        # self.scale = scale
+        self.scale = scale
         self.clip_func = clipping_function
         self.graph = graph
+        # Indicator for how verbose the EA should be 3 is print everything --> debug. and 0 is no output
         self.log_level = 0
 
+        # Use a argument clipping function if available
         if self.clip_func is not None:
             self.clip()
 
+    def set_population(self, pop):
+        self.population = pop
+        self.pop_size = len(pop)
+
+    # Do not use
     def _log(self, message, level):
         if level <= self.log_level:
             print(message)
 
+    # for printing important messages
     def log(self, message):
         self._log(message, 1)
 
+    # For printing informational messages
     def info(self, message):
         self._log(message, 2)
 
+    # For printing trace info. i.e telling you precisely what the algorithm is doing.
     def trace(self, message):
         self._log(message, 3)
 
+    # Runs the EA for i iterations
     def run(self, iterations: int):
-        
+        # Array for storing the fitnesses for all iterations
         fitnessess = np.zeros((iterations, self.pop_size))
         for i in range(iterations):
             
             self.log("#### iteration {} ####".format(i))
+
+            # Evaluate individuals and store fitness values
             fitness = self.evaluation()
             fitnessess[i] = fitness
             self.selection(fitness)
             self.reproduction()
             self.mutation()
 
-            if self.graph:
-                plt.figure(2)
-                plt.subplot(111)                                
-                plt.cla()
-                averages = np.average(fitnessess, 1)
-                plt.plot(averages[:i+1])                
+            # Show the average and max fitness values per iterationif self.graph:
+            plt.figure(2)
+            plt.subplot(111)
+            plt.cla()
+            min_fit = np.min(fitnessess, 1)
+            averages = np.average(fitnessess, 1)
+            plt.plot(averages[:i+1])
+            plt.plot(min_fit)
+            # plt.subplot(111)
+            try:
                 plt.pause(1e-40)
+            except TclError:
+                pass
 
         return fitnessess
 
+    # Evaluation of the population
     def evaluation(self):
-        
+
         size = len(self.population)
-        fitness = np.zeros(size)
-        for i in range(size):
-            self.trace("Evaluating individual {}: {}".format(i, self.population[i]))
-            fitness[i] = self.fit_func(self.population[i])
-            self.trace("Fitness:{}".format(fitness[i]))
+
+        if True:
+            fitness = np.array(_async_evaluate(self.population, self.fit_func))
+        else:
+            for i in range(size):
+                fitness = np.zeros(size)
+                self.trace("Evaluating individual {}: {}".format(i, self.population[i]))
+                fitness[i] = self.fit_func(self.population[i])
+                self.trace("Fitness:{}".format(fitness[i]))
         self.info("Average fitness: {}".format(np.average(fitness)))
         return fitness
 
+
+
+    # Truncated ranked selection
+    # TODO: Encapsulate this!
     def selection(self, fitness):
         n = 8
         order = np.argsort(fitness)
@@ -82,13 +136,14 @@ class EvolutionaryAlgorithm:
         self.fit_func(self.population[order[-1]], plot=True)
         self.population = self.population[order]
 
-
+    # Copy remaining population until population is at pop_size
     def reproduction(self):
         size = len(self.population)
         repeats = np.ceil(self.pop_size/size)
         pop = np.repeat(self.population, repeats, axis=0)
         self.population = pop[:self.pop_size]
 
+    # clips the population into the allowable ranges
     def clip(self):
         self.population = np.apply_along_axis(func1d=self.clip_func, axis=1, arr=self.population)
 
@@ -111,9 +166,10 @@ class EvolutionaryAlgorithm:
             self.population[parents[1]] = ind2
 
         # mutation
+        # TODO Change this make the chance of evolving 0.2 for all instead of for each
         mutation_rate = 0.2
         x = np.random.random(self.pop_size)
-        mask = np.argwhere(x < mutation_rate)        
+        mask = np.argwhere(x < mutation_rate)
         cols = np.random.choice(10,len(mask),replace=True)
         mutations = np.random.normal(scale=self.scale[cols])
       
@@ -122,3 +178,20 @@ class EvolutionaryAlgorithm:
             self.clip()
 
 
+
+
+if __name__ == '__main__':
+    from eval import process_arguments
+    from simulation import polymer
+    from data_processing import minMaxNorm
+    from simulation import polymer
+
+    diff = minMaxNorm("polymer_20k.xlsx", polymer )
+    alg = EvolutionaryAlgorithm(10, diff.get_difference, process_arguments)
+    alg.log_level = 3
+    print(alg.run(10))
+    for ind in alg.population:
+        dat = polymer(*process_arguments(ind))
+        plt.plot()
+    plt.pause(-1)
+    print(alg.population)
